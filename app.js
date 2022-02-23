65008265acaf14ae8ba90068760b3d044947e86f
jorjclem <97485184+jorjclem@users.noreply.github.com>
2022-02-22 20:41:49 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+let list = [
+    {
+        name: "Caution",
+        sound: 1
+    },
+    {
+        name: "Dying Breed",
+        sound: 3
+    },
+    {
+        name: "Human",
+        sound: 8
+    },
+    {
+        name: "Spaceman",
+        sound: 7
+    },
+    {
+        name: "Sam's Town",
+        sound: 5
+    },
+    {
+        name: "Bright side",
+        sound: 4
+    }
+];
+
+
+for(let i=0; i<list.length; i++){
+    let ele =document.createElement("div");
+    let nameEle = document.createElement("h1");
+    let soundEle = document.createElement("h3");
+    nameEle.innerHTML=list[i].name;
+    soundEle.innerHTML=list[i].sound;
+    ele.body.appendChild(nameEle);
+    ele.body.appendChild(soundEle);
+    document.body.appendChild(ele);
+}
+
+let number = 10;
+
+let button= document.createElement("button");
+button.innerHTML="click"+number;
+button.addEventListener("click", function(){
+   number=number+1;
+   button.innerHTML="click"+number;
+})
+
+if (number=== 15){
+    alert ("we hit the number")
+}
+document.body.appendChild(button);
+document.body.appendChild(ele);
+
